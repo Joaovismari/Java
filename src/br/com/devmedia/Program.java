f80a79398459b71a260491b7f57d5dcd62631d39
João Marcos Vismari de Lacerda <jmvismari@hotmail.com>
2020-06-16 17:42:11 -0300

    16.06

--- src/br/com/devmedia/Program.java
+++ src/br/com/devmedia/Program.java
@@ -2,7 +2,18 @@ package br.com.devmedia;
 
 public class Program {
     public static void main(String[] args) {
+        //exemplo de instanciação de classe simples em java, utilizando método da classe HelloWorld
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.sayHello();
+
+        String cor ="Verde";
+
+        if (cor.equals("Verde")){
+            System.out.println("Andar");
+        }else {
+            System.out.println("Parar e Aguardar");
+        }
+
+
     }
 }
