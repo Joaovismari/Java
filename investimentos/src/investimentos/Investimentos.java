921c845608b5118235e76642c21dd6704c262d2c
João Marcos Vismari de Lacerda <jmvismari@hotmail.com>
2021-03-29 17:35:00 -0300

    Update Investimentos.java

--- investimentos/src/investimentos/Investimentos.java
+++ investimentos/src/investimentos/Investimentos.java
@@ -1,6 +1,4 @@
-
 package investimentos;
-
 
 public class Investimentos {
 
@@ -8,7 +6,12 @@ public class Investimentos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Estão Prontas Crianças ?");
+        int HorasTotais = 60;
+        double SalarioHora = 10.0;
+        double pagamento = (HorasTotais - 44.0) * SalarioHora;
+        System.out.println(pagamento);
     }
+
     
+
 }
