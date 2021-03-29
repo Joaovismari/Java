85d84f51df746b77a3e268a56119ebce73973b22
João Marcos Vismari de Lacerda <jmvismari@hotmail.com>
2021-03-29 16:33:02 -0300

    teste
    teste

---
+++ investimentos/src/investimentos/Investimentos.java
@@ -0,0 +1,14 @@
+
+package investimentos;
+
+
+public class Investimentos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Estão Prontas Crianças ?");
+    }
+    
+}
