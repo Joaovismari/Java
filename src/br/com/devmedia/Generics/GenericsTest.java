1232fd12693a34072943d6d168b4d7a94a100296
João Marcos Vismari de Lacerda <jmvismari@hotmail.com>
2021-05-22 20:42:20 -0300

    introdução a Generics e WildCard

---
+++ src/br/com/devmedia/Generics/GenericsTest.java
@@ -0,0 +1,18 @@
+package br.com.devmedia.Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsTest {
+    public static void main(String[] args) {
+        //type erasure
+        List<String> lista = new ArrayList<>();
+        lista.add("Dinheiro");
+        lista.add("Segurança");
+        lista.add("Saúde");
+
+        for (String obj:lista) {
+            System.out.println(obj);
+        }
+    }
+}
